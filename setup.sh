f4cd069cbd352043d1281024a0017620b053a8e8
Hongzi Mao <hongzi@mit.edu>
2020-01-09 13:29:54 -0500

    ipython and graphviz

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@ sudo apt update
 sudo apt install -y python3-pip
 pip3 install --user numpy
 pip3 install --user torch
+pip3 install --user IPython
 pip3 install --user networkx
+pip3 install --user graphviz
 pip3 install --user matplotlib
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 echo 'export PATH=$HOME/.cargo/bin:$PATH' >> ~/.bashrc
