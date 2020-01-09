8506d4e773bab55109d990ee39eddeffdd0f7d16
Hongzi Mao <hongzi@mit.edu>
2020-01-09 13:17:29 -0500

    add in set up code

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install -y python3-pip
+pip3 install --user numpy
+pip3 install --user torch
+pip3 install --user networkx
+pip3 install --user matplotlib
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
