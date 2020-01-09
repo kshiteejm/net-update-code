00b3454ed786a947413693581f3da055b6e80674
Hongzi Mao <hongzi@mit.edu>
2020-01-09 13:35:46 -0500

    scipy

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 sudo apt update
 sudo apt install -y python3-pip
 pip3 install --user numpy
+pip3 install --user scipy
 pip3 install --user torch
 pip3 install --user IPython
 pip3 install --user networkx
