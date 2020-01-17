06cd4bc6cdcef380a8e16040b76b290ccc302d21
Hongzi Mao <hongzi@mit.edu>
2020-01-17 12:06:56 -0500

    test rl model and unit test env

--- environment/unit_test/test_env.py
+++ environment/unit_test/test_env.py
@@ -14,10 +14,12 @@ class TestEnv(unittest.TestCase):
     def test_env_an_episode(self):
         env = RLEnv()
         state = env.reset()
-        node_feats_torch, adj_mats_torch, switch_mask_torch = get_tensors(state)
+        node_feats_torch, adj_mats_torch, switch_mask_torch = \
+            get_tensors(*state)
         for (i, s) in enumerate(switch_mask_torch.numpy()[0]):
             if s:
                 state, reward, done = env.step(i)
 
-        node_feats_torch, adj_mats_torch, switch_mask_torch = get_tensors(state)
+        node_feats_torch, adj_mats_torch, switch_mask_torch = \
+            get_tensors(*state)
         assert(sum(switch_mask_torch.numpy()[0]) == 1)
--- test_rl_model.py
+++ test_rl_model.py
@@ -1,23 +1,42 @@
-import config
+import sys
+import torch
+from param import config
+from environment.rl_interface import RLEnv
+from torch.distributions import Categorical
+from utils.state_transform import get_tensors
+from gcn.batch_mgcn_policy import Batch_MGCN_Policy
 
-def main(model_dir, n_epoch):
-    # policy network for taking actions and policy gradient
+
+def test_model(policy_net):
+    env = RLEnv()
+    state = env.reset()
+    node_feats, adj_mats, switch_mask = state
+    done = False
+    while not done:
+        node_feats_torch, adj_mats_torch, switch_mask_torch = \
+            get_tensors(node_feats, adj_mats, switch_mask)
+
+        switch_log_pi, switch_pi, masked_pi = policy_net(
+            node_feats_torch, adj_mats_torch, switch_mask_torch)
+
+        # sample action
+        switch_p = Categorical(masked_pi)
+        switch_a = switch_p.sample().item()
+
+        next_state, reward, done = env.step(switch_a)
+        node_feats, adj_mats, switch_mask = next_state
+
+        print(switch_a, reward)
+
+
+if __name__ == '__main__':
+    n_epoch = sys.argv[1]
+
     policy_net = Batch_MGCN_Policy(
         config.num_switches, [config.class_feat, config.path_feat,
         config.link_feat, config.switch_feat], config.n_output,
         config.hid_dim, config.h_size, config.n_steps)
-    state_dicts = torch.load("%s/policy_net_epoch_%s" % (model_dir, n_epoch))
+    state_dicts = torch.load(config.result_folder + "policy_net_epoch_{}".format(n_epoch))
     policy_net.load_state_dict(state_dicts)
 
-    # value network for value prediction and advantage estimation
-    value_net = Batch_MGCN_Value(
-        config.num_switches, [config.class_feat, config.path_feat,
-        config.link_feat, config.switch_feat], config.n_output,
-        config.hid_dim, config.h_size, config.n_steps)
-    state_dicts = torch.load("%s/value_net_epoch_%s" % (model_dir, n_epoch))
-    value_net.load_state_dict(state_dicts)
-
-if __name__ == '__main__':
-    model_dir = sys.argv[1]
-    n_epoch = sys.argv[2]
-    main(model_dir, n_epoch)
+    test_model(policy_net)
