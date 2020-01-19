e4d736fd71068517b374df58a7e62818dd9d2233
kshiteejm <kshiteej.mahajan@gmail.com>
2020-01-19 14:13:16 -0500

    test trained value model

--- test_rl_model.py
+++ test_rl_model.py
@@ -7,10 +7,11 @@ from param import config
 from environment.rl_interface import RLEnv
 from torch.distributions import Categorical
 from utils.state_transform import get_tensors
+from gcn.batch_mgcn_value import Batch_MGCN_Value
 from gcn.batch_mgcn_policy import Batch_MGCN_Policy
 
 
-def test_model(policy_net):
+def test_model(policy_net, value_net):
     env = RLEnv()
     state = env.reset()
     node_feats, adj_mats, switch_mask = state
@@ -19,17 +20,25 @@ def test_model(policy_net):
         node_feats_torch, adj_mats_torch, switch_mask_torch = \
             get_tensors(node_feats, adj_mats, switch_mask)
 
+        values = value_net(
+            node_feats_torch, adj_mats_torch)
+        
         switch_log_pi, switch_pi, masked_pi = policy_net(
             node_feats_torch, adj_mats_torch, switch_mask_torch)
 
-        # sample action
-        switch_p = Categorical(masked_pi)
-        switch_a = switch_p.sample().item()
+        # # sample action
+        # switch_p = Categorical(masked_pi)
+        # switch_a = switch_p.sample().item()
+        
+        print("manually enter action: ")
+        switch_a = int(input())
 
         next_state, reward, done = env.step(switch_a)
         node_feats, adj_mats, switch_mask = next_state
 
+        print(masked_pi.detach().item())
         print(switch_a, reward)
+        print(values.detach().item())
 
 def get_optimal_action():
     env = RLEnv()
@@ -45,8 +54,7 @@ def get_optimal_action():
     os.system("cat /tmp/action_seq.csv")
 
 if __name__ == '__main__':
-    n_epoch = sys.argv[1]
-
+    torch.manual_seed(config.seed)
     np.random.seed(config.seed)
     random.seed(config.seed)
 
@@ -54,8 +62,17 @@ if __name__ == '__main__':
         config.num_switches, [config.class_feat, config.path_feat,
         config.link_feat, config.switch_feat], config.n_output,
         config.hid_dim, config.h_size, config.n_steps)
-    state_dicts = torch.load(config.result_folder + "policy_net_epoch_{}".format(n_epoch))
-    policy_net.load_state_dict(state_dicts)
+    
+    value_net = Batch_MGCN_Value(
+        config.num_switches, [config.class_feat, config.path_feat,
+        config.link_feat, config.switch_feat], config.n_output,
+        config.hid_dim, config.h_size, config.n_steps)
+    
+    if config.saved_policy_model is not None:
+        policy_net.load_state_dict(torch.load(config.saved_policy_model))
+
+    if config.saved_value_model is not None:
+        value_net.load_state_dict(torch.load(config.saved_value_model))
 
     test_model(policy_net)
-    get_optimal_action()
+    # get_optimal_action()
