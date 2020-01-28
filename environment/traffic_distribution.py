90524b69a3e9bb3f219fd0b6d5c02f5d3148d567
kshiteejm <kshiteej.mahajan@gmail.com>
2020-01-28 18:51:17 -0500

    gravity model

--- environment/traffic_distribution.py
+++ environment/traffic_distribution.py
@@ -2,11 +2,11 @@ import random
 import numpy as np
 
 class TrafficDistribution:
-    def __init__(self, num_endpoints):
+    def __init__(self, num_endpoints, total_traffic, endpoint_max):
         self.num_endpoints = num_endpoints
 
     def uniform(self, mean_min=1250, mean_max=2500, spread=625):
-        avg_tor_pair_traffic = random.randint(mean_min, mean_max)
+        avg_tor_pair_traffic = np.random.randint(mean_min, mean_max+1)
         minima = avg_tor_pair_traffic - spread
         maxima = avg_tor_pair_traffic + spread
         num_endpoints = self.num_endpoints
@@ -15,5 +15,20 @@ class TrafficDistribution:
             for dst in range(num_endpoints):
                 if src == dst:
                     continue
-                traffic_matrix[src][dst] = float(random.randint(minima, maxima))
+                traffic_matrix[src][dst] = float(np.random.randint(minima, maxima+1))
+        return traffic_matrix
+
+    def gravity_model(self, total_traffic):
+        num_endpoints = self.num_endpoints
+        T = total_traffic
+        T_in = np.random.exponential(2, [num_endpoints])
+        T_out = np.random.exponential(2, [num_endpoints])
+        p_in = T_in/sum(T_in)
+        p_out = T_out/sum(T_out)
+        traffic_matrix = np.zeros((num_endpoints, num_endpoints))
+        for src in range(num_endpoints):
+            for dst in range(num_endpoints):
+                if src == dst:
+                    continue
+                traffic_matrix[src][dst] = T * p_in[src] * p_out[dst]
         return traffic_matrix
--- unit_test.py
+++ unit_test.py
@@ -10,6 +10,7 @@ from gcn.unit_test.test_param import TestParams
 from environment.unit_test.test_env import TestEnv
 from utils.unit_test.test_rewards import TestRewards
 from environment.unit_test.test_dcn_env import TestDCNEnv
+from environment.unit_test.test_traffic_matric import TestTrafficMatrix
 
 
 if __name__ == '__main__':
